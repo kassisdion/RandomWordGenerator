cae6b2904f060f2c00b0be8c5a38052c7906f558
florian <florian.faisant@gmail.com>
2016-05-11 03:10:46 +0900

    add markov algo

--- src/com/kassisdion/Main.java
+++ src/com/kassisdion/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            String inputFileName = "testInput/COMPOUND.TXT";
+            String inputFileName = "media/testInput/COMPOUND.TXT";
             char[] tokens = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
             ProbabilityTableGenerator probabilityTableGenerator = new ProbabilityTableGenerator(inputFileName, tokens);
@@ -18,8 +18,10 @@ public class Main {
             probabilityTableGenerator.buildTable();
             probabilityTableGenerator.printReadableTable();
 
-            MarkovChain markovChain = new MarkovChain(probabilityTableGenerator.getTable());
-            markovChain.generateWord();
+            MarkovChain markovChain = new MarkovChain(probabilityTableGenerator.getTable(), tokens);
+            for (int i=0; i < 10; i++) {
+                System.out.println("word :" + markovChain.generateWord(3));
+            }
 
         } catch (FileNotFoundException e) {
             System.out.println("Invalid or no input file");
--- src/com/kassisdion/MarkovChain/MarkovChain.java
+++ src/com/kassisdion/MarkovChain/MarkovChain.java
@@ -1,13 +1,39 @@
 package com.kassisdion.MarkovChain;
 
+
+import java.util.Random;
+
 public class MarkovChain {
     private final double[][] mProbabilityTable;
+    private final char[] mTokens;
+    private final Random mRandom;
 
-    public MarkovChain(double[][] probabilityTable) {
+    public MarkovChain(double[][] probabilityTable, final char[] tokens) {
         mProbabilityTable = probabilityTable;
+        mTokens = tokens;
+        mRandom = new Random();
     }
 
-    public String generateWord() {
-        return "";
+    public String generateWord(final int size) {
+        int state = mRandom.nextInt(mTokens.length);
+
+        String word = "";
+
+        //run Markov chain
+        while (word.length() < size) {
+            double r = Math.random();
+            double sum = 0.0;
+
+            for (int j=0; j < mProbabilityTable[state].length; j++) {
+                sum += mProbabilityTable[state][j];
+                if (r <= sum) {
+                    word += mTokens[j];
+                    state = j;
+                    break;
+                }
+            }
+        }
+
+        return word;
     }
 }
